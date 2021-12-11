5f3ad8bc8b585493038fe3999114e05965794d2a
Mr nUUb <josef.schabasser@gmx.at>
2021-12-11 20:38:44 +0100

    add second test case

--- tests/react.test.tsx
+++ tests/react.test.tsx
@@ -10,8 +10,16 @@ import { QRImage } from '../src/react'
 const text = 'I ❤️ @mr-nuub/qr-image'
 
 it('renders correctly', () => {
+  const { container } = render(<QRImage value={text} />)
+  const canvas = container.firstChild as HTMLCanvasElement
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+  const events = ctx.__getEvents()
+  expect(events).toMatchSnapshot()
+})
+
+it('renders with custom settings', () => {
   const { container } = render(
-    <QRImage value={text} ecLevel='M' size={256} margin={2} fgColor='maroon' bgColor='#EEEEEE' className='' />,
+    <QRImage value={text} ecLevel='L' size={128} margin={1} fgColor='maroon' bgColor='#EEEEEE' className='my-class' />,
   )
   const canvas = container.firstChild as HTMLCanvasElement
   const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
